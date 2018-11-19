792acde2a424d4bd3cc8d4df05c44e8109f2b227
Bryan Wong <bryanwong7788@gmail.com>
2018-11-19 11:17:37 -0800

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get "portfolio/:id/edit", to: "portfolios#edit", as: "portfolio_edit"
