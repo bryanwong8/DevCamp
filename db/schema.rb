23ee4a2f3a6575c4db20dbac83f4f7f8d8ed6144
Bryan Wong <bryanwong7788@gmail.com>
2018-12-30 17:53:04 -0800

    Implemented link to topics and buit ot topic views

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_12_24_050651) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
+    t.bigint "blogs_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "blog_id"
